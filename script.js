ab2bc365e68ab61ce5ea9dc3302f5df0f4962c0d
ugg <ugkirin@gmail.com>
2022-01-29 23:31:18 +0900

    もっぎそんの型作成完了

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict'
+const song = document.querySelector(".song");
+const play = document.querySelector(".play");
+const replay = document.querySelector(".replay");
+const outline = document.querySelector(".moving-outline circle");
+const video = document.querySelector(".vid-container video");
+//Sounds
+const sounds = document.querySelectorAll(".sound-picker button");
+//Time Display
+const timeDisplay = document.querySelector(".time-display");
+const outlineLength = outline.getTotalLength();
+//Duration
+const timeSelect = document.querySelectorAll(".time-select button");
+let fakeDuration = 600;
+
+// しほ
+
+
+// りゅういち
+
+
+// ひがみの
+
+
+// りゅういち
+
+
+// ひがみの
+
+
+
+// ゆうじ
+
+
