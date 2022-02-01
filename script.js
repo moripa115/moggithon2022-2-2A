a2e7b374cd251dd135a44434afd1c110c35f3094
あなたの名前 <>
2022-02-01 15:50:23 +0900

    自分の範囲のhtmlとcss完成

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const video = document.querySelector(".vid-container video");
 const sounds = document.querySelectorAll(".sound-picker button");
 //Time Display
 const timeDisplay = document.querySelector(".time-display");
-const outlineLength = outline.getTotalLength();
+// const outlineLength = outline.getTotalLength();
 //Duration
 const timeSelect = document.querySelectorAll(".time-select button");
 let fakeDuration = 600;
@@ -17,7 +17,18 @@ let fakeDuration = 600;
 
 
 // りゅういち
-
+// play.addEventListener('click', ()=>{
+//     play.classList.toggle('aaa')
+// });
+// if (id_play.classList.contains('aaa')) {
+//     // song.play();
+//     video.play();
+//     play.src = "./svg/pause.svg"}
+//   } else {
+//     song.pause();
+//     video.pause();
+//     play.src = "./svg/play.svg";
+//   }
 
 // ひがみの
 
