356115152ea3298b3786ab67185dcb4c507faefd
Your Name <you@example.com>
2022-02-03 16:01:35 +0900

    js:checkplaying以外完了、html:type=button追加

--- script.js
+++ script.js
@@ -19,47 +19,66 @@ let fakeDuration = 600;
 // りゅういち
 sounds.forEach(sound => {
     sound.addEventListener("click", function() {
-      song.src = this.getAttribute("data-sound");
-      video.src = this.getAttribute("data-video");
-      checkPlaying(song);
+        song.src = this.getAttribute("data-sound");
+        video.src = this.getAttribute("data-video");
+        checkPlaying(song);
     });
-  });
+});
 
 // ひがみの
 
+play.addEventListener("click", function() {
+    checkPlaying(song);
+});
+
+replay.addEventListener("click", function() {
+    restartSong(song);
+});
+
+const restartSong = song => {
+    let currentTime = song.currentTime;
+    song.currentTime = 0;
+    console.log("ciao");
+}
 
 // りゅういち
 timeSelect.forEach(option => {
     option.addEventListener("click", function() {
-      fakeDuration = this.getAttribute("data-time");
-      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration = this.getAttribute("data-time");
+        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
         fakeDuration % 60
       )}`;
     });
-  });
+});
 
 // ひがみの
 
-
-
-// ゆうじ
-song.ontimeupdate=function(){
-    let currentTime=song.currentTime;
-    let elapsed=fakeDuration-currentTime;
-    let minutes=Math.floor(elapsed/60);
-    let seconds=Math.floor(elapsed%60);
-    timeDisplay.textContent=`${minutes}:${seconds}`;
-    let progress=outlineLength-(currentTime/fakeDuration)*outlineLength;
-    outline.style.storokeDashoffset=progress;
-
-    if(currentTime>=fakeDuration){
+const checkPlaying = song => {
+    if (song.paused) {
+        song.play();
+        video.play();
+        //play.src = "./svg/pause.svg";→display:noneでやってみる(消すを消すみたいなやつ)
+    } else {
         song.pause();
-        song.currentTime=0;
-        play.src="./svg/play.svg";
         video.pause();
+        //play.src = "./svg/play.svg";→display:noneでやってみる
     }
 };
 
+// ゆうじ
+song.ontimeupdate = function() {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let minutes = Math.floor(elapsed / 60);
+    let seconds = Math.floor(elapsed % 60);
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.storokeDashoffset = progress;
 
-
-
+    if (currentTime >= fakeDuration) {
+        song.pause();
+        song.currentTime = 0;
+        play.src = "./svg/play.svg";
+        video.pause();
+    }
+};
